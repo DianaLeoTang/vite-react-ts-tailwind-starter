21e13ea17f9eb2f55e52b828352eb3218a2248dc
19801191517 <tangwangyao0228@163.com>
2025-05-21 18:51:34 +0800

    feat: 调整head展示，增加国际化配置this-is-medmentorx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,6 @@
 import React, { ReactNode } from 'react'
 import { LanguageSelector } from '../language-selector'
-import { Button } from '../ui/button'
 import { useTranslation } from 'react-i18next'
-import { Github } from 'lucide-react'
 
 interface IProps {
   leftNode?: ReactNode
@@ -12,16 +10,12 @@ export function Header(props: IProps) {
 
   return (
     <div className="fixed left-0 top-0 flex w-full items-center justify-between border bg-slate-50 bg-opacity-70 px-4 py-4 md:px-12">
-      <a href="/" className="text-xs md:text-base">
+      {/* <a href="/" className="text-xs md:text-base">
         Vite React TS Tailwind Starter
-      </a>
+      </a> */}
+      <div>{t('this-is-medmentorx')}</div>
       <div className="flex items-center gap-4">
         <LanguageSelector />
-        <Button size={'icon'} asChild className="rounded-full">
-          <a href="https://github.com/Quilljou/vite-react-ts-tailwind-starter" target="_blank" rel="noreferrer">
-            <Github />
-          </a>
-        </Button>
       </div>
     </div>
   )
