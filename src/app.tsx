facfaa2d8de593f87b95006c0f6230105a6c216e
19801191517 <tangwangyao0228@163.com>
2025-05-21 18:20:07 +0800

    feat: 调整Helmet组件的引入方式

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,25 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2025-05-21 16:37:48
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /vite-react-ts-tailwind-starter/src/app.tsx
+ */
 import React, { useMemo } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import { createRouter } from './router'
 
 export default function App() {
   const queryClient = useMemo(() => new QueryClient({}), [])
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={createRouter()} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={createRouter()} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </HelmetProvider>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,20 @@
-import { Helmet } from 'react-helmet'
+/*
+ * @Author: Diana Tang
+ * @Date: 2025-05-21 16:37:48
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /vite-react-ts-tailwind-starter/src/pages/home/index.tsx
+ */
+import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
-import { Hero } from 'src/components/hero'
+import { Hero } from '@/components/hero'
 
 export default function Home() {
   const { t } = useTranslation('translation')
   return (
     <>
       <Helmet>
-        <title>{t('title')}</title>
+        <title>Home</title>
       </Helmet>
       <Hero />
     </>
